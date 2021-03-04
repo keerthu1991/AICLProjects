0fe18c6250c284cec871d124956bdc43c46e4ed1
keerthu1991 <keerthanambika03@gmail.com>
2021-03-04 22:39:52 +0530

    phpday9

---
+++ AICLTestFile.php
@@ -0,0 +1,18 @@
+<html>
+<head><h1>First PHP</h1>
+<body>
+<?PHP
+echo "Hello this is PHP";
+
+$a=90;
+$b=78;
+
+$d=$a + $b;
+echo $d;
+
+print "hiii."
+
+?>
+</body>
+
+</html>
---
+++ TestProgram.php
@@ -0,0 +1,8 @@
+<html>
+<head><h1>Test</h1></head>
+<body>
+<?php
+print "xampp"
+?>
+</body>
+</html>
