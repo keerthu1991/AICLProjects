2dbbf96a08bcc5684392b15834dd18d407544d78
keerthu1991 <keerthanambika03@gmail.com>
2021-03-12 17:32:03 +0530

    Update aiclformtable.php

--- aiclformtable.php
+++ aiclformtable.php
@@ -14,6 +14,8 @@ if ($conn->connect_error) {
 // sql to create table
 $sql = "CREATE TABLE AICLformTable(
 firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(30) NOT NULL,
 contact VARCHAR(10) NOT NULL,
 messagetext VARCHAR(50) NOT NULL
 )";
